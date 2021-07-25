9a8160781fd9296a77dfbbe0600891795a832a81
Niraj Kumar <nirajkumar.123580@gmail.com>
2021-07-25 18:09:39 +0530

    Ready_2nd

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,19 +1,31 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super() ;
+        this.state = {
+            title: 'Phone',
+            price: 999,
+            qty: 1,
+            img: ''
+        }
+    }
     render(){
+        const {title, price, qty} = this.state;
         return (
           <div className="cart-item">
               <div className="left-block">
-                  <img style={ styles.image } />
+                  <img style={ styles.image } alt="product" src="https://rukminim1.flixcart.com/image/416/416/k2jbyq80pkrrdj/mobile-refurbished/z/a/f/iphone-11-pro-max-256-u-mwhm2hn-a-apple-0-original-imafkg2ftc5cze5n.jpeg?q=70"/>
               </div>
               
               <div className="right-block">
-                  <div style={ { fontSize: 17 , fontFamily: "sans-serif" } }>Phone</div>
-                  <div style={ { fontSize: 12 , fontFamily: "sans-serif" } }>₹ 999</div>
-                  <div style={ { fontSize: 12 , fontFamily: "sans-serif"} }>Qty: 1</div>
+                  <div style={ { fontSize: 17 , fontFamily: "sans-serif" } }>{title}</div>
+                  <div style={ { fontSize: 11 , fontFamily: "sans-serif" } }>₹ {price}</div>
+                  <div style={ { fontSize: 11 , fontFamily: "sans-serif"} }>Qty: {qty}</div>
                   <div className="cart-item-actions">
-                      { /* Buttons */ }
+                      <img src="https://img-premium.flaticon.com/png/512/1665/premium/1665629.png?token=exp=1627203038~hmac=fc00135dff5cc960976ba03ef95f4de9" className="action-icons" alt="inc" />
+                      <img src="https://img-premium.flaticon.com/png/512/1665/premium/1665663.png?token=exp=1627203121~hmac=f85eeae42ac9ec82edb6183dc596ae55" className="action-icons" alt="dec" />
+                      <img src="https://image.flaticon.com/icons/png/512/3096/3096673.png" className="action-icons" alt="del" />
                   </div>
               </div>
           </div>  
@@ -25,7 +37,7 @@ class CartItem extends React.Component{
 const styles = {
     image:{
       height:110,
-      width:110,
+      width:85,
       borderRadius: 3,
       backgroundColor: "#ccc"
     }
